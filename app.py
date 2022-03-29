9be79e58556691a6e334d6302c665e1ba9e36288
Silja <44512829+Ronttikasa@users.noreply.github.com>
2022-03-29 21:05:36 +0300

    Add registration

---
+++ app.py
@@ -0,0 +1,9 @@
+from os import getenv
+from flask import Flask
+
+app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+import routes
---
+++ routes.py
@@ -0,0 +1,34 @@
+from app import app
+from db import db
+from flask import render_template, request, redirect
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    username = request.form["username"]
+    password = request.form["password"]
+    password_again = request.form["password_again"]
+    role = 1
+    
+    #TODO: errors
+    if not username or len(username) > 20:
+        print("username not valid")
+    elif not password == password_again:
+        print("the passwords differ")
+    elif len(password) < 8:
+        print("password too short")
+    else:
+        password_hash = generate_password_hash(password)
+        sql = "INSERT INTO users (username, password, role) \
+            VALUES (:name, :password, :role)"
+        db.session.execute(sql, {"name": username, "password": password_hash, "role":role})
+        db.session.commit()        
+
+    return redirect("/")
+    
