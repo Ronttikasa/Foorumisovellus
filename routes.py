f86e881bbfbba40a2cc1a5671364cd0222fde229
Silja <44512829+Ronttikasa@users.noreply.github.com>
2022-04-02 15:33:05 +0300

    Add registration

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from app import app
 from db import db
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, session
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
