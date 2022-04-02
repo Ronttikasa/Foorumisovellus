857372267fc6711953114c4b2b769e997cb5cca6
Silja <44512829+Ronttikasa@users.noreply.github.com>
2022-04-02 15:49:10 +0300

    Add login, logout basic functionality

--- routes.py
+++ routes.py
@@ -9,20 +9,25 @@ from werkzeug.security import check_password_hash, generate_password_hash
 def index():
     return render_template("index.html")
 
-@app.route("/register", methods=["POST"])
+@app.route("/register")
 def register():
+    return render_template("register.html")
+
+@app.route("/register_user", methods=["POST"])
+def register_user():
     username = request.form["username"]
     password = request.form["password"]
     password_again = request.form["password_again"]
     role = 1
     
-    #TODO: errors
+    # TODO username already exists (error handling)
+    # TODO error messages in app
     if not username or len(username) > 20:
-        print("username not valid")
+        print("käyttäjätunnus ei kelpaa")
     elif not password == password_again:
-        print("the passwords differ")
+        print("salasanat eroavat")
     elif len(password) < 8:
-        print("password too short")
+        print("salasana liian lyhyt")
     else:
         password_hash = generate_password_hash(password)
         sql = "INSERT INTO users (username, password, role) \
@@ -30,5 +35,29 @@ def register():
         db.session.execute(sql, {"name": username, "password": password_hash, "role":role})
         db.session.commit()        
 
+    return redirect("/")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    sql = "SELECT password FROM users WHERE username=:name"
+    result = db.session.execute(sql, {"name": username})
+    user = result.fetchone()
+
+    if not user:
+        print("käyttäjää ei löydy")
+        return redirect("/")
+    if not check_password_hash(user[0], password):
+        print("väärä salasana")
+        return redirect("/")
+
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
     return redirect("/")
     
