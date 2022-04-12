85b7a815798e28a464abfc0ae587d3f80f526639
Silja <44512829+Ronttikasa@users.noreply.github.com>
2022-04-12 21:55:40 +0300

    Update sql schema

--- schema.sql
+++ schema.sql
@@ -9,5 +9,33 @@ CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     content TEXT,
     user_id INTEGER REFERENCES users,
-    time TIMESTAMP
+    thread INTEGER REFERENCES threads,
+    time TIMESTAMP,
+    starts_thread INTEGER
+);
+
+CREATE TABLE threads (
+    id SERIAL PRIMARY KEY,
+    header TEXT,
+    category_id INTEGER REFERENCES categories
+);
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    category_name TEXT
+);
+
+CREATE TABLE groups (
+    id SERIAL PRIMARY KEY,
+    group_name TEXT
+);
+
+CREATE TABLE users_in_groups (
+    user_id INTEGER REFERENCES users,
+    group_id INTEGER REFERENCES groups
+);
+
+CREATE TABLE category_access (
+    category_id INTEGER REFERENCES categories,
+    group_id INTEGER REFERENCES groups
 );
